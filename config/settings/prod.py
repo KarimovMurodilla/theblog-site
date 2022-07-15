66a9c614f34c548acc5dea4f4d4349fdc3dde00b
KarimovMurodilla <karimovmurodilla15@gmail.com>
2022-07-15 17:25:58 +0500

    some changes

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -34,9 +34,10 @@ AWS_HEADERS = {
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # Heroku Logging
 LOGGING = {
